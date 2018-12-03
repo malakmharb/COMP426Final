748957e8bdae003286eb6d6f7617131a87c237b1
Malak Harb <mharb1@live.unc.edu>
2018-12-03 14:33:50 -0500

    Add files via upload
    log in/signup front end

---
+++ login.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    var url_base = "https://wwwp.cs.unc.edu/Courses/comp426-f17/users/marinzam/Budgie/";
+    $('#login_button').on('click',
+    function (e) {
+        e.preventDefault();
+
+        $.ajax(url_base + "/login.php",
+            {type: "POST",
+            dataType: "json",
+            data: JSON.stringify({
+                UserID: $("#input_email_login").val(),
+                Password: $("#input_password_login").val()
+            }),
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(response) {
+                console.log(response);
+            }});
+    }); 
+
+    $('#signup_button').on('click',
+    function (e) {
+        e.preventDefault();
+
+        $.ajax(url_base + "/CreateUser.php",
+            {type: "POST",
+            dataType: "json",
+            data: JSON.stringify({
+                FirstName : $("#input_first_name").val(),
+                LastName: $("#input_last_name").val(),
+                UserID: $("#input_email_signup").val(),
+                Password: $("#input_password_signup").val()
+            }),
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(response) {
+                console.log(response);
+            }});
+    }); 
+
+}); 
